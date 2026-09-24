1a9ec42ac02c4f59eecefbe0bb1c0412c1889983
cladrum <cladrum@gmail.com>
2026-09-24 19:43:59 +0200

    seconda lezione

--- m1/esercizi/Helloaudio/Program.cs
+++ m1/esercizi/Helloaudio/Program.cs
@@ -1,1 +1,8 @@
 ﻿Console.WriteLine("porcoddio");
+int numeroVoci = 8;
+float volumeMaster = 0.75f;
+float ratioCompressore = 2.5f;
+
+Console.WriteLine(numeroVoci);
+Console.WriteLine(volumeMaster);
+Console.WriteLine(ratioCompressore);
---
+++ m1/esercizi/metronomo/Program.cs
@@ -0,0 +1,24 @@
+﻿int bpm = 120;
+float secondiPerBattito = 60f / bpm;
+float secondiPerCroma = secondiPerBattito/2;
+float secondiPerSemicroma = secondiPerBattito/4;
+
+Console.WriteLine   (secondiPerBattito);
+Console.WriteLine(secondiPerCroma);
+Console.WriteLine(secondiPerSemicroma);
+
+Console.WriteLine($"La Croma è di {secondiPerCroma} secondi");
+Console.WriteLine($"La Semicrome è di {secondiPerSemicroma} secondi");
+
+float db = -20f;
+float lineare = MathF.Pow(10, db / 20);
+
+Console.WriteLine(lineare);
+
+float LinearToDb(float ValoreLineare)
+{
+    return 20 * MathF.Log10(ValoreLineare);
+}
+
+float dbDiRitorno = LinearToDb(lineare);
+Console.WriteLine(dbDiRitorno);
